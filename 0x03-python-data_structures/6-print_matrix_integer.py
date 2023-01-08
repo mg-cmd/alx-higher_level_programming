7d7acfd7256815dd6490f757a479b8850eefd1cd
mg-cmd <gmebrahtu8@gmail.com>
2023-01-08 10:49:12 -0800

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix != [[]]:
-        for row in matrix:
-            for elem in row:
-                print("{:d}".format(elem), end=" "
-                                                                                                      if elem != row[-1] else '\n')
-    else:
+    for row in matrix:
+        for column in row:
+            print("{:d}".format(column), sep='', end='')
+            if (column != row[len(row) - 1]):
+                print(' ', end='')
         print()
